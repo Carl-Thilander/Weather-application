8884212919256723a7702f47d76322035155d566
carl-thilander <carl.thilander@medieinstitutet.se>
2025-08-28 09:58:27 +0200

    Added two tests in cypress (TDD)

--- cypress/e2e/todo.cy.ts
+++ cypress/e2e/todo.cy.ts
@@ -17,3 +17,29 @@ describe("todo", () => {
     cy.contains("Feed the cat").should("not.exist");
   });
 });
+
+
+//////Actual tests for weather application
+describe("weather app", () => {
+  beforeEach(() => {
+    cy.task("reseed");
+  });
+
+  it("should display the weather for a default city", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("h2").should("contain.text", "Weather in New York");
+    cy.get(".temperature").should("contain.text", "15°C");
+    cy.get(".condition").should("contain.text", "Cloudy");
+  });
+
+  it("should be able to search for a different city", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("[data-cy='Enter city']").type("Kuala Lumpur");
+    cy.get("[data-cy='Search-city']").click();
+    cy.get("h1").should("contain.text", "Weather in Kuala Lumpur");
+    cy.get(".temperature").should("contain.text", "28°C");
+    cy.get(".condition").should("contain.text", "Sunny");
+  });
+
+}
+);
