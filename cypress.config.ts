8d86a8eac5636e7bee3b3cab202608c8128867a7
Carl Thilander <carl.thilander@medieinstitutet.se>
2025-09-02 13:53:20 +0200

    Added cross-spawn. test environment works

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,4 @@
-import { spawn } from "child_process";
+import { spawn } from "cross-spawn";
 import { defineConfig } from "cypress";
 import { MongoMemoryReplSet } from "mongodb-memory-server";
 import waitOn from "wait-on";
